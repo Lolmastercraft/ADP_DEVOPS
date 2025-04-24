f192b10b7dca6e1f27a254b09b26974a563fba8e
breadwbutter <al07003220@tecmilenio.mx>
2025-04-23 22:32:26 -0600

    commit emergencia

--- Infraestructura/AVANCE DEL PROYECTO.tf
+++ Infraestructura/AVANCE DEL PROYECTO.tf
@@ -1,224 +1,151 @@
-#AVANCE DEL PROYECTO
-
-#====================PROVEDOR====================
+#====================PROVEEDOR====================
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 #====================VPC====================
 resource "aws_vpc" "vpc_pro" {
-    cidr_block = "10.13.0.0/20"
+  cidr_block = "10.13.0.0/20"
 
-    tags = {
-        Name = "VPC-Pro"
-    }
+  tags = {
+    Name = "VPC-Pro"
+  }
 }
 
-#====================SUBRED PUBLICA====================
+#====================SUBRED PÚBLICA====================
 resource "aws_subnet" "subred_pub" {
-    vpc_id = aws_vpc.vpc_pro.id
-    cidr_block = "10.13.0.0/24"
-    map_public_ip_on_launch = true
+  vpc_id                   = aws_vpc.vpc_pro.id
+  cidr_block               = "10.13.0.0/24"
+  map_public_ip_on_launch  = true
 
-    tags = {
-        Name = "Subred-Pub"
-    }
+  tags = {
+    Name = "Subred-Pub"
+  }
 }
 
 #====================GATEWAY====================
 resource "aws_internet_gateway" "gateway_pro" {
-    vpc_id = aws_vpc.vpc_pro.id
-
-    tags = {
-        Name = "Gateway-Pro"
-    }
-}
-
-#====================TABLAS DE RUTA PUBLICA====================
-resource "aws_route_table" "tablaruta_pub" {
-    vpc_id = aws_vpc.vpc_pro.id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.gateway_pro.id
-    }
-
-    tags = {
-        Name = "TablaRuta-Pro"
-    }
-}
-
-#==========ASOCIACION DE TABLAS DE RUTAS==========
-resource "aws_route_table_association" "Asociaciones_Pro" {
-    subnet_id = aws_subnet.subred_pub.id
-    route_table_id = aws_route_table.tablaruta_pub.id
-}
-
-#====================SUBRED PRIVADA====================
-resource "aws_subnet" "subred_pri" {
-    vpc_id = aws_vpc.vpc_pro.id
-    cidr_block = "10.13.1.0/24"
-    map_public_ip_on_launch = false
-
-    tags = {
-        Name = "Subred-Privada"
-    }
-}
-
-#==============NAT Gateway=============
-resource "aws_eip" "nat_eip" {
-  vpc = true
-}
-
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.subred_pub.id
+  vpc_id = aws_vpc.vpc_pro.id
 
   tags = {
-    Name = "NAT-Gateway-Pro"
+    Name = "Gateway-Pro"
   }
 }
 
-#===============TABLA DE RUTA PRIVADA===============
-resource "aws_route_table" "tablaruta_priv" {
+#====================TABLA DE RUTA PÚBLICA====================
+resource "aws_route_table" "tablaruta_pub" {
   vpc_id = aws_vpc.vpc_pro.id
 
   route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway_pro.id
   }
 
   tags = {
-    Name = "TablaRuta-Priv"
+    Name = "TablaRuta-Pro"
   }
 }
 
-#=============ASOCIACION DE TABLA DE RUTA PRIVADA
-resource "aws_route_table_association" "priv_association" {
-  subnet_id      = aws_subnet.subred_pri.id
-  route_table_id = aws_route_table.tablaruta_priv.id
+#==========ASOCIACIÓN DE TABLA DE RUTA PÚBLICA==========
+resource "aws_route_table_association" "asoc_pub" {
+  subnet_id      = aws_subnet.subred_pub.id
+  route_table_id = aws_route_table.tablaruta_pub.id
 }
 
 
-#======================CREACION DE GRUPOS DE SEGURIDAD======================
+#======================GRUPOS DE SEGURIDAD======================
 
-#====================SG JUMP SERVER WINDOWS====================
+# SG PARA JUMP SERVER WINDOWS
 resource "aws_security_group" "SG_JS_WIN" {
-    vpc_id = aws_vpc.vpc_pro.id
-    name = "SG_JS_WIN"
-    description = "SG para jump server con OS Windows"
+  vpc_id      = aws_vpc.vpc_pro.id
+  name        = "SG_JS_WIN"
+  description = "SG para jump server con OS Windows"
 
-    #REGLAS DE ENTRADA==========
-    
-    #RDP
-    ingress {
-        from_port = 3389
-        to_port = 3389
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] #IPv4 de la pc
-    }
-    
-    #REGLAS DE SALIDA==========
-    #All TRAFIC
-    egress{
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }  
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-#====================SG LINUX WEB SERVER====================
-resource "aws_security_group" "SG-LIN-WEB" {
-    vpc_id = aws_vpc.vpc_pro.id
-    name = "SG-LIN-WEB"
-    description = "SG para servidor web Linux"
+# SG PARA LINUX WEB SERVER
+resource "aws_security_group" "SG_LIN_WEB" {
+  vpc_id      = aws_vpc.vpc_pro.id
+  name        = "SG_LIN_WEB"
+  description = "SG para servidor web Linux"
 
-    #REGLAS DE ENTRADA==========
-    #SSH
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] #IPv4 del JUMP
-    }
+  # SSH desde Jump Server
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    #HTTP
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  # HTTP público
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    #REGLAS DE SALIDA====================
-    #HTTP
-    egress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port   = 443
-        to_port     = 443
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-#====================CREACION DE INSTANCIAS====================
 
-#INSTANCIA DE Windows Jump Server====================
-resource "aws_instance" "Win-JS" {
-    ami = "ami-0c765d44cf1f25d26"
+#====================INSTANCIAS====================
 
-    instance_type = "t2.medium"
+# Jump Server Windows
+resource "aws_instance" "Win_JS" {
+  ami                         = "ami-0c765d44cf1f25d26"
+  instance_type               = "t2.medium"
+  subnet_id                   = aws_subnet.subred_pub.id
+  vpc_security_group_ids      = [aws_security_group.SG_JS_WIN.id]
+  associate_public_ip_address = true
+  key_name                    = "vockey"
 
-    subnet_id = aws_subnet.subred_pub.id
-
-    vpc_security_group_ids = [aws_security_group.SG_JS_WIN.id]
-
-    associate_public_ip_address = true
-
-    key_name = "vockey"
-
-    tags = {
-        Name = "Servidor Windows Jump Server"
-    }
+  tags = {
+    Name = "Servidor Windows Jump Server"
+  }
 }
 
-#INSTANCIA DE Linux Web Server
-resource "aws_instance" "Lin-Web" {
-    ami = "ami-084568db4383264d4"
-    
-    instance_type = "t2.micro"
+# Linux Web Server
+resource "aws_instance" "Lin_Web" {
+  ami                         = "ami-084568db4383264d4"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.subred_pub.id       
+  vpc_security_group_ids      = [aws_security_group.SG_LIN_WEB.id]
+  associate_public_ip_address = true                            
+  key_name                    = "vockey"
 
-    subnet_id = aws_subnet.subred_pri.id
-
-    vpc_security_group_ids = [aws_security_group.SG-LIN-WEB.id]
-
-    associate_public_ip_address = false
-
-    key_name = "vockey"
-
-    tags = {
-        Name = "Linux Web Server"
-    }
-
+  tags = {
+    Name = "Linux Web Server"
+  }
 }
+
 
 #====================OUTPUTS====================
 
-#OUTPUT JUMP SERVER====================
 output "public_ipWinJS" {
-    description = "IP public del Windows Jump Server"
-    value = aws_instance.Win-JS.public_ip
+  description = "IP pública del Windows Jump Server"
+  value       = aws_instance.Win_JS.public_ip
 }
 
-#OUTPUT LINUX WEB====================
 output "public_ipLinWeb" {
-    description = "IP public del Linux Web Server"
-    value = aws_instance.Lin-Web.public_ip
+  description = "IP pública del Linux Web Server"
+  value       = aws_instance.Lin_Web.public_ip
 }
